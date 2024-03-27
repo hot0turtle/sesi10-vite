ff6ad2478cc86b54b590880f8a4ec9720398be1c
hot0turtle <leofarel2015@gmail.com>
2024-03-27 09:56:14 -0300

    endobras

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Tilt } from 'react-tilt'
 
-import Card from './components/Card'
-import img01 from "./assets/imgs/foto1.jpg"
-import img02 from "./assets/imgs/foto2.jpg"
-import img03 from "./assets/imgs/foto3.jpg"
+import Card from "./components/Card"
+import Menu from "./components/Menu"
+import Foto01 from "./assets/imgs/foto1.jpg"
+import Foto02 from "./assets/imgs/foto2.jpg"
+import Foto03 from "./assets/imgs/foto3.jpg"
 
-import style from './App.module.css'
+import style from "../src/App.module.css"
 
 function App() {
 
   const defaultOptions = {
-    reverse:        false,  // reverse the tilt direction
+    reverse:        true,  // reverse the tilt direction
     max:            15,     // max tilt rotation (degrees)
     perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
     scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
@@ -20,40 +21,35 @@ function App() {
     reset:          true,    // If the tilt effect has to be reset on exit.
     easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
   }
-  
-  return (
-    <div className={style.section01}>
-      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-        <Card 
-        title="Paisagem 01"
-        image={img01}
-        text1="descricao da paisagem 01"
-        text2="Curitiba/PR"
-        />
-      </Tilt>
 
-      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-      <Card 
-        title="Paisagem 02"
-        image={img02}
-        text1="descricao da paisagem 02"
-        text2="Guaratuba/PR"
+  return (
+    <>
+      <Menu/>
+      <section className={style.section1}>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+          <Card title="Paisagem 01" 
+                  image={Foto01} 
+                  text1="Texto sobre a paisagem" 
+                  text2="Curitiba/PR"
+            />
+        </Tilt>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+        <Card title="Paisagem 02" 
+              image={Foto02} 
+              text1="Texto sobre a paisagem 02" 
+              text2="Guaratuba/PR"
         />
-      </Tilt>
-      
-      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-      <Card 
-          title="Paisagem 03"
-          image={img03}
-          text1="descricao da paisagem 03"
-          text2="Colombo/PR"
-          />
-      </Tilt>
-  
-     
-     
-      </div>
-      
+        </Tilt>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+
+            <Card title="Paisagem 03" 
+              image={Foto03} 
+              text1="Texto sobre a paisagem 03" 
+              text2="Colombo/PR"
+        />
+          </Tilt>
+      </section>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+import { Contact } from './Contact.jsx';
+import { Calc } from './Calc.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App />,
+  },
+  {
+    path: "/contact",
+    element:<Contact />,
+  },
+  {
+    path: "/calc",
+    element:<Calc />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
