6f819cc16e8b0f361f041d2c90e2af8672137a20
hot0turtle <leofarel2015@gmail.com>
2024-03-27 08:24:20 -0300

    hey walter

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+- Baixar o node 20.11.1 pelo botao no site de dowload do lts node
+- npm create vite@latest nomeDoProjeto -- --template react (aqui cria o projeto em vite co react)
+- npm install (instala a pasta node-modules no proejto)
+- npm run dev (inicia o projeto no browser)
+
+- conceitos basicos
+componentes
+propriedades
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Tilt } from 'react-tilt'
+
+import Card from './components/Card'
+import img01 from "./assets/imgs/foto1.jpg"
+import img02 from "./assets/imgs/foto2.jpg"
+import img03 from "./assets/imgs/foto3.jpg"
+
+import style from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            15,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
+  
   return (
-    <>
-      <h1>Sapo de chapeu</h1>
-    </>
+    <div className={style.section01}>
+      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+        <Card 
+        title="Paisagem 01"
+        image={img01}
+        text1="descricao da paisagem 01"
+        text2="Curitiba/PR"
+        />
+      </Tilt>
+
+      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+      <Card 
+        title="Paisagem 02"
+        image={img02}
+        text1="descricao da paisagem 02"
+        text2="Guaratuba/PR"
+        />
+      </Tilt>
+      
+      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+      <Card 
+          title="Paisagem 03"
+          image={img03}
+          text1="descricao da paisagem 03"
+          text2="Colombo/PR"
+          />
+      </Tilt>
+  
+     
+     
+      </div>
+      
   )
 }
 
